b704b708961432575e24a405f7a9944a5478eff6
Melwyn Turbant <melwyn@mimpix.co.uk>
2021-11-25 23:26:20 +0100

    :recycle: Add favicon + content description in index.html

--- src/App.js
+++ src/App.js
@@ -1,17 +1,15 @@
 import './index.css';
 import React from 'react';
 import {
-  BrowserRouter as Router, Routes, Route, Link,
+  BrowserRouter as Router, Routes, Route,
 } from 'react-router-dom';
 import Home from './routes/Home';
 import Search from './routes/Search';
+import Header from './routes/Header';
 
 const App = () => (
   <Router>
-    <nav>
-      <Link to="/">Home</Link>
-      <Link to="/search">Search</Link>
-    </nav>
+    <Header />
     <Routes>
       <Route path="/" element={<Home />} />
       <Route path="/search" element={<Search />} />
