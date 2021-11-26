28ad4798a30cef2d76dc254944b050ce3eb3fa3c
Melwyn Turbant <melwyn@mimpix.co.uk>
2021-11-26 17:01:54 +0100

    :bug: Include link with default subreddit "javascript"

--- src/App.js
+++ src/App.js
@@ -12,7 +12,7 @@ const App = () => (
     <Header />
     <Routes>
       <Route path="/" element={<Home />} />
-      <Route path="/search/javascript" element={<Search />} />
+      <Route path="/search" element={<Search />} />
     </Routes>
   </Router>
 );
--- src/routes/Header.js
+++ src/routes/Header.js
@@ -8,7 +8,7 @@ const Header = () => (
   <header>
     <ul className="nav-list">
       <li className="nav-item"><Link to="/" className="logo"><img src={logo} alt={logo} /></Link></li>
-      <li className="nav-item"><Link to="/search">Search</Link></li>
+      <li className="nav-item"><Link to="/search/javascript">Search</Link></li>
       <li className="nav-item"><Link to="/#how-it-works">How it works</Link></li>
       <li className="nav-item"><Link to="/#about">About</Link></li>
     </ul>
