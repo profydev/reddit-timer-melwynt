f7af41f812555b8fa56d5b56921d1461ae3c1bf0
Melwyn Turbant <melwyn@mimpix.co.uk>
2021-11-26 16:56:23 +0100

    :bug: Use <header> tag

--- src/routes/Header.js
+++ src/routes/Header.js
@@ -5,14 +5,14 @@ import {
 import logo from '../logo.svg';
 
 const Header = () => (
-  <navbar>
+  <header>
     <ul className="nav-list">
       <li className="nav-item"><Link to="/" className="logo"><img src={logo} alt={logo} /></Link></li>
       <li className="nav-item"><Link to="/search">Search</Link></li>
       <li className="nav-item"><Link to="/#how-it-works">How it works</Link></li>
       <li className="nav-item"><Link to="/#about">About</Link></li>
     </ul>
-  </navbar>
+  </header>
 );
 
 export default Header;
