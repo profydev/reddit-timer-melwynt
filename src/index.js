f5a48565b5f63748e3b0c9642db48f2d3e2d4a06
Melwyn <melwynt@gmail.com>
2022-05-21 00:00:16 +0200

    Header (#5)
    * :rocket: Search url points to search page w/ default value
    
    * :bug: Use <header> tag
    
    * :bug: Include link with default subreddit "javascript"
    
    * :sparkles: Create header with logo and nav links

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { render } from 'react-dom';
-import './index.css';
+import './sass/main.scss';
 import App from './App';
 
 const rootElement = document.getElementById('root');
