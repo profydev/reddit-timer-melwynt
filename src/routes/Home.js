49fc388d7f6a630fd0cf09e04dfd1192938be0ae
Mel <melwynt@gmail.com>
2022-05-21 10:47:46 +0200

    :sparkles: Add hero section

--- src/routes/Home.js
+++ src/routes/Home.js
@@ -1,8 +1,21 @@
 import React from 'react';
+import preview from '../images/preview.png';
 
 const Home = () => (
   <main>
-    <h2>Home</h2>
+    <h1>No reactions to your reddit posts?</h1>
+    <p>
+      Great timing, great results! Find the best time to post on your subreddit.
+    </p>
+    <div className="button">
+      <a href="/">Show me the best time</a>
+    </div>
+    <div className="search">
+      <a href="https://www.reddit.com/r/javascript/">r/javascript</a>
+    </div>
+    <div className="screenshot">
+      <img src={preview} alt="" />
+    </div>
   </main>
 );
 
