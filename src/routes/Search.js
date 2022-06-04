69c590fe78fecb5d58c6c0ef91a78ed0059a1115
Mel <melwynt@gmail.com>
2022-06-04 11:51:21 +0200

    :sparkles: Load posts from Reddit

--- src/routes/Search.js
+++ src/routes/Search.js
@@ -1,25 +1,103 @@
-import React, { useState, useEffect } from 'react';
-import { useParams, useNavigate } from 'react-router-dom';
+import React, { useEffect, useReducer } from 'react';
+import { useParams, useNavigate, useLocation } from 'react-router-dom';
+
+import axios from 'axios';
+
+const getData = async (word) => {
+  console.log('fetching data...');
+
+  const url = `https://www.reddit.com/r/${word}/top.json?t=2022&limit=100`;
+  const data = await axios.get(`${url}`);
+  return data;
+};
+
+const reducer = (state, action) => {
+  switch (action.type) {
+    case 'getPosts':
+      console.log(`data getPosts: ${action.data}`);
+      return {
+        ...state,
+        posts: action.data,
+        searchFlag: action.searchFlag,
+      };
+    case 'setSearch':
+      if (action.refresh && state.searchFlag) {
+        console.log(`data setSearch: ${action.data}`);
+        return {
+          ...state,
+          search: action.search,
+          posts: action.data,
+        };
+      }
+      return {
+        ...state,
+        search: action.search,
+      };
+    case 'searchFlag':
+      return {
+        ...state,
+        searchFlag: true,
+      };
+    default:
+      return state;
+  }
+};
 
 const Search = () => {
   const { subreddit } = useParams();
-  const [search, setSearch] = useState(subreddit);
-
+  const location = useLocation();
   const navigate = useNavigate();
+
+  const initialState = {
+    search: subreddit || 'javascript',
+    posts: {},
+    searchFlag: true,
+  };
+  const [posts, dispatch] = useReducer(reducer, initialState);
 
   const handleSubmit = (e) => {
     e.preventDefault();
-    navigate(`/search/${search}`);
+    navigate(`/search/${posts.search}`);
+
+    const fetchData = async () => {
+      const data = await getData(posts.search);
+      return data;
+    };
+
+    fetchData().then((data) => {
+      dispatch({ type: 'getPosts', data, searchFlag: false });
+    });
   };
 
   const handleChange = (e) => {
     e.preventDefault();
-    setSearch(e.target.value);
+    dispatch({ type: 'setSearch', search: e.target.value, refresh: false });
   };
 
   useEffect(() => {
-    setSearch(subreddit);
-  }, [subreddit]);
+    console.log('subreddit: ', subreddit);
+    console.log('location: ', location);
+
+    const fetchData = async () => {
+      const data = await getData(subreddit);
+      return data;
+    };
+
+    if (posts.searchFlag) {
+      fetchData().then((data) => {
+        dispatch({
+          type: 'setSearch',
+          search: subreddit,
+          data,
+          refresh: true,
+        });
+      });
+    }
+
+    return () => {
+      dispatch({ type: 'searchFlag', searchFlag: true });
+    };
+  }, [subreddit, location, posts.searchFlag]);
 
   return (
     <main className="main-search">
@@ -31,13 +109,14 @@ const Search = () => {
             <input
               id="search"
               type="text"
-              value={search}
+              value={posts.search}
               onChange={handleChange}
             />
           </label>
           <button type="submit">Search</button>
         </form>
       </div>
+      <div>{posts.search}</div>
     </main>
   );
 };
