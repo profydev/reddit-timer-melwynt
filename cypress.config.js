4cf02e6ef6bce207258927a0872a832dc03ec99c
Melwyn <melwynt@gmail.com>
2022-06-15 17:37:33 +0200

    Fix eslint for cypress.config.js

--- cypress.config.js
+++ cypress.config.js
@@ -1,3 +1,4 @@
+// eslint-disable-next-line import/no-extraneous-dependencies
 const { defineConfig } = require('cypress');
 
 module.exports = defineConfig({
@@ -7,6 +8,7 @@ module.exports = defineConfig({
     // We've imported your old cypress plugins here.
     // You may want to clean this up later by importing these.
     setupNodeEvents(on, config) {
+      // eslint-disable-next-line global-require
       return require('./cypress/plugins/index.js')(on, config);
     },
     baseUrl: 'http://localhost:3000',
