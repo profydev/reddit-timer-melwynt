aa33f64d55f959516e573a247f951f15096660b8
Melwyn <melwynt@gmail.com>
2022-06-15 18:41:54 +0200

    Clean cypress config file

--- cypress.config.js
+++ cypress.config.js
@@ -7,10 +7,12 @@ module.exports = defineConfig({
   e2e: {
     // We've imported your old cypress plugins here.
     // You may want to clean this up later by importing these.
+    /*
     setupNodeEvents(on, config) {
       // eslint-disable-next-line global-require
       return require('./cypress/plugins/index.js')(on, config);
     },
+    */
     baseUrl: 'http://localhost:3000',
   },
 });
--- cypress/plugins/index.js
+++ cypress/plugins/index.js
@@ -1,2 +1,2 @@
 // eslint-disable-next-line no-unused-vars
-module.exports = (on, config) => {};
+// module.exports = (on, config) => {};
